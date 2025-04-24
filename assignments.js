8ec49ae29f92839c5e461a163ccb5d41d3c0fd57
dumikx <duma.aalexandru@yahoo.com>
2025-04-24 09:59:52 +0300

    fisiere noi

---
+++ assignments.js
@@ -0,0 +1,28 @@
+const country = 'Romania'
+const continent = "Europe"
+let population = 19
+let isIsland = false
+const language = 'Romana'
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+let halfPopulation = (population / 2)
+halfPopulation++
+console.log(halfPopulation)
+console.log(population > 6)
+console.log(population < 33)
+
+
+const description = `${country} is in europe and its ${population} milion people speak ${language}`
+
+console.log(description)
+
+if (population > 33) {
+    console.log(`${country} population is above average`)
+
+} else {
+    console.log(`${country} population is ${33 - population} below average`)
+
+}
