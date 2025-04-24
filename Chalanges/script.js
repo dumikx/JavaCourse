9014d3bf1f17da8cd3f894189ec3f8dc88569828
dumikx <duma.aalexandru@yahoo.com>
2025-04-24 10:02:55 +0300

    fisiere noi din nou

---
+++ Chalanges/script.js
@@ -0,0 +1,30 @@
+//data 1
+
+let massMark = 78;
+let massJohn= 92;
+const heightMark=1.69;
+const heightJohn=1.95;
+
+let bmiMark=massMark/heightMark **2;
+let bmiJohn=massJohn/heightJohn **2;
+const markHigherBMI=bmiMark>bmiJohn;
+
+console.log(bmiMark);
+console.log(bmiJohn);
+console.log(markHigherBMI);
+
+//data 2 
+/*
+let massMark = 95;
+let massJohn= 88;
+const heightMark=1.88;
+const heightJohn=1.76;
+
+let bmiMark=massMark/heightMark **2;
+console.log(bmiMark);
+
+let bmiJohn=massJohn/heightJohn **2;
+console.log(bmiJohn);
+
+const markHigherBMI=bmiMark>bmiJohn;
+console.log(markHigherBMI);*/
