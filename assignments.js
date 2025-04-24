25babbb9fa40e2a77a4c8a78924f1ed598192dc1
dumikx <duma.aalexandru@yahoo.com>
2025-04-24 14:10:26 +0300

    lesson 18

--- assignments.js
+++ assignments.js
@@ -26,3 +26,9 @@ if (population > 33) {
     console.log(`${country} population is ${33 - population} below average`)
 
 }
+
+console.log (`9`-`5`)
+console.log(`19`-`13`+`17`)
+console.log(`19`-`13`+ 17)
+console.log(`123`<57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
