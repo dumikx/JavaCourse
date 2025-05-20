58a9b4b1c02aec2e6ec2bf845e44b318e0a6ebd1
dumikx <duma.aalexandru@yahoo.com>
2025-05-20 22:15:46 +0300

    011-2:36

--- Fundamentals part 2/script.js
+++ Fundamentals part 2/script.js
@@ -92,18 +92,44 @@ console.log(yearUntilRetirement(1950, `Mike`));
 
 */
 
-const friend1 = `Michael`;
+/*const friend1 = `Michael`;
 const friend2 = `Steven`;
 const firend3 = `Peter`;
 
 const friends = [`Michael`, 'Steven', 'Peter'];
 console.log(friends);
 
-const years = new Array(1991, 1984, 2008, 2020);
-console.log(years)
+const y = new Array(1991, 1984, 2008, 2020);
+console.log(y)
 
 console.log(friends[0]);
 console.log(friends[2]);
 
 console.log(friends.length)
 console.log(friends[friends.length - 1])
+
+friends[2] = 'Jay'
+console.log(friends)
+
+const firstname = 'Jonas'
+const jonas = [firstname, 'Prenume', 2037 - 1991, 'teacher', friends]
+console.log(jonas)
+console.log(jonas.length)
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+console.log(ages); */
+
+const friends = ['Michael', 'Steven', 'Peter']
+friends.push('Jay');
+console.log(friends)
